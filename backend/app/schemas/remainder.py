a2331b42a871707051ad89939beeb0965ceac36e
vanika02 <vanikadangi02@gmail.com>
2026-02-06 11:27:13 +0000

    pydantic schema

---
+++ backend/app/schemas/remainder.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class RemainderCreate(BaseModel):
+    title: str
+    description: str | None = None
+    remaind_at: datetime
+
+class RemainderResponse(BaseModel):
+    id: int
+    is_active: bool
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
