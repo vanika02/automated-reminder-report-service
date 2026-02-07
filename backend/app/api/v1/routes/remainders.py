1f728fe7a22a3fc165d9f363304539761df082d5
vanika02 <vanikadangi02@gmail.com>
2026-02-07 01:16:16 +0000

    async route done

--- backend/app/api/v1/routes/remainders.py
+++ backend/app/api/v1/routes/remainders.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, BackgroundTasks
 from sqlalchemy.orm import Session
 from app.db.database import SessionLocal
 from app.schemas.remainder import RemainderCreate, RemainderResponse
@@ -21,3 +21,13 @@ def create(remainder: RemainderCreate, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[RemainderResponse])
 def list_all(db: Session = Depends(get_db)):
     return remainder_service.list_remainders(db)
+
+@router.post("/", response_model=RemainderResponse)
+async def create(remainder: RemainderCreate, db: Session = Depends(get_db), backgorund_tasks = BackgroundTasks):
+    result = remainder_service.create_remainder(db, remainder.dict())
+
+    backgorund_tasks.add_task(
+        remainder_service.log_remainder_created,
+        result.id
+    )
+    return result
