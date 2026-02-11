162183ce266be7f81ab664b5f1e104a74d251252
vanika02 <vanikadangi02@gmail.com>
2026-02-11 10:59:13 +0000

    cors added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from app.api.v1.routes import remainders, analytics
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Remainder Service")
 
 app.include_router(remainders.router, prefix="/api/v1")
 app.include_router(analytics.router, prefix="/api/v1")
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
